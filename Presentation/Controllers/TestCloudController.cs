8e40145387ed4becd6587d77c642725336d4ee23
juancruze <juancruzdeangelis1@gmail.com>
2025-11-17 19:06:26 -0300

    TestCloud Controller

---
+++ Presentation/Controllers/TestCloudController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Http.HttpResults;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Presentation.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TestCloudController : ControllerBase
+    {
+
+        [HttpGet]
+        public IActionResult Ping()
+        {
+            return Ok("Pong from Cloud Controller");
+        }
+    }
+}
